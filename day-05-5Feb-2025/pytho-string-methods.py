ed17e2a6f3e185214fdb0b2f43f3bb4dd14ea752
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-07 00:33:50 +0500

    day five work

---
+++ day-05-5Feb-2025/pytho-string-methods.py
@@ -0,0 +1,93 @@
+
+# name="hamza"
+
+# print(name)
+# print(type(name))
+
+# para=''' dakjfldakjflaf
+# lasjflkajlkj
+# ajsljfdalf
+# lajsfdlkaj'''
+# print(para)
+
+# print("'hello wrold'")
+# print('"hello wrold"')
+
+# print("hello \ world ")
+# print('\\Users\\DECENT LAPTOPS\\Desktop\\python\\day-05')
+# print('hello \nworld')
+# print('hello \t world')
+
+# indexing and slicing in python string
+
+# name="sufyan"
+# print(name[-8])
+
+# # print name in reverse order
+# name="sufyan"
+# reverse_name=""  # n a
+
+# for i in range(5,-1,-1):
+#     reverse_name=reverse_name+name[i]
+
+# print(f"given name is : {name}") 
+# print(f"name in revese ordre is : {reverse_name}") 
+
+
+
+# print name in reverse order make the programe dynaic to the user input
+# name=input("Enter your name here to reverse it's order : ") #hamza 
+# reverse_name=""  # n a
+
+# for i in range((len(name)-1),-1,-1):
+#     reverse_name=reverse_name+name[i]
+
+# print(f"given name is : {name}") 
+# print(f"name in revese ordre is : {reverse_name}")
+
+
+# String  Slicing in python
+# text="Weclome to the python programe "
+
+# print(text[2:8])
+
+
+
+# .3 String Concatenation & Repetition
+
+# first_name="ali"
+# second_name="khan"
+# full_name=first_name+second_name
+# print(full_name)
+
+# using join() method
+# repetition of string
+# print(first_name * 10)
+
+#  String Formatting Techniques
+
+# name="hamza"
+# city="peshawar"
+# idnumber="053092"
+
+# print("your name is {} and your city is {} your id number is {}".format(city,name,idnumber))
+
+
+# strip method in python 
+# name=input("Enter your name here")
+# if name.strip()=="hamza":
+#     print("yes i know who you are ")
+
+# searching in string  
+
+# language="python is a programming language"
+
+# print(language.find("python"))
+# print(language.index("is"))
+# print(language.startswith("f"))
+# print(language.endswith("f"))
+
+# String replacement in python
+# message="i love java"
+# updated_message=message.replace("java", "Python")
+# print(updated_message)

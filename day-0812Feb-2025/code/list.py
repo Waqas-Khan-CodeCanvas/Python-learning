0e3c837e3116f6d12599e85ce4493d8e300f93f8
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-08 22:41:49 +0500

    day eight

---
+++ day-0812Feb-2025/code/list.py
@@ -0,0 +1,60 @@
+# list in detailed in python
+
+# syntax  list_name=[elecments , elements .. ..]
+
+# names=["ali","hamza","sufyan","zaryab","zaryan","zarar"]
+# number=[1,2,3,4,5,6,7,8]
+# mix_list=["ali",True,2,3,4,5]
+
+# print(names)
+# print(number)
+# print(mix_list)
+
+# list_zerors=[0]
+# print(list_zerors*100)
+
+# list indexing 
+# names=["ali","hamza","sufyan","zaryab","zaryan","zarar"]
+
+# print(names[0])
+# print(names[-2])
+
+# list slicing in python
+# names=["ali","hamza","sufyan","zaryab","zaryan","zarar"]
+
+# print(names[1:5])
+# print(names[:5])
+# print(names[:])
+# print(names[: : 2])
+# print(names[: : -1])
+
+
+
+# nested list in python
+# number=[[1,2,3,4],[5,6,7,8,9],[10,20,30,40,50]]
+
+# print(number[1])
+# print(number[-2])
+# print(number[1:2])
+
+# print(number[1][0])
+
+# loop on nested list in python
+# number=[[1,2,3,4],[5,6,7,8,9],[10,20,30,40,50]]
+
+# numbers=[1,2,3,4,5,6,7,8,9]
+# for col in number: #i=[1,2,3,4]
+#     for row in col:
+#         print(row)
+
+# for col in number: #i=[1,2,3,4]
+#     for row in col:
+#         if row%2!=0:
+#             print(row)
+         
+
+
+
+ 
+
+

8fba9845402dc1d4a02dd3ef81a2c3ec8ef46f17
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-10 02:31:57 +0500

    day 15 of python

---
+++ day-15-22Feb-2025/question-solution.py
@@ -0,0 +1,21 @@
+# # Question 1: Even or Odd Checker
+# # Write a function even_odd_checker(n) that takes an integer n and prints "Even" if the number is even, and "Odd" if the number is odd. Use a loop to check and print the result for numbers from 1 to n.
+
+# def even_odd_checker(n):
+#     result=""
+#     if n%2 ==0:
+#         result="Even"
+#     else:
+#         result="odd"
+#     return result
+
+
+# while True:
+#     userInput=int(input("Enter a number to check that is even or odd ( for exit -1) :"))
+#     result=even_odd_checker(userInput)        
+#     print(result)
+#     if userInput == -1:
+#         print("Thank you")
+#         exit()
+    
+        

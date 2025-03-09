776be5ce81e6891a69d70101e26483c9e658b2ca
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-09 15:14:55 +0500

    day 12

---
+++ day-12-17Feb-2025/function.py
@@ -0,0 +1,18 @@
+
+# function in python first lecture
+
+# def greet():
+#     print("I'm a Function")
+# greet()  
+
+
+
+def myInfo():
+    print("my name is Waqas khan")
+    print("I'm a programer")
+    print("I'm from mardan ")
+    
+    
+myInfo()    
+myInfo()   
+    
---
+++ day-12-17Feb-2025/remning_list_concepts.py
@@ -0,0 +1,70 @@
+# remove a specific element from the list
+# names=["ali","waqas","hamza","sufyan","zaryab"]
+# print(names)
+# user_input=input("Enter value that you want to remove from the list : ") # hamza
+
+# print(names.index(user_input))
+# element_index=names.index(user_input)
+# print(names[element_index])
+# if names.remove(names[element_index])!="none":
+#     print(names)
+# else:
+#     print("not found")  
+
+
+# for name in names:  #["ali","waqas","hamza","sufyan","zaryab"]
+#     if name==user_input:
+#         names.remove(user_input)
+        
+# print(names)
+
+
+# how to update a specific element in the list 
+
+# names=["ali","waqas","hamza","sufyan","zaryab"]
+# print(names)
+# list_element=input("What you want to replace : ") 
+# user_element=input("with what you want to replace : ") 
+
+# element_index=names.index(list_element)
+# names[element_index]=user_element
+
+# print(names)
+
+
+# sorting of the list in python
+# names=["ali","waqas","hamza","sufyan","zaryab"]
+# names.sort()
+# print(names)
+# names.sort(reverse=True)
+# print(names)
+
+# number=[2,4,6,8,9,8,6,4,2,3,1]
+# number.sort()
+# print(number)
+# print(number.count(2))
+
+# number.sort(reverse=True)
+# print(number)
+
+
+
+# find the max and min of the list in python
+# number=[2,4,6,8,9,8,6,4,2,3,-1]
+# list_max=number[0]
+# list_min=number[0]
+
+# for i in number:
+#     if list_max < number[i]:
+#         list_max=number[i]
+#     if list_min > number[i]:
+#         list_min=number[i]    
+
+# print(f"max of the list is : {list_max}")
+# print(f"min of the list is : {list_min}")
+
+# fint the average of the list in python
+# this is your task
+# number=[2,4,6,8,9,8,6,4,2,3,1]
+
+# print()

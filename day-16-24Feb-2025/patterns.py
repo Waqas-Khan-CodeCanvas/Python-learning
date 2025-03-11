484cdb5122c5ece01a2ce1d9825ccf3b9b71a1cb
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-11 19:54:23 +0500

    day 16 of python'

---
+++ day-16-24Feb-2025/patterns.py
@@ -0,0 +1,13 @@
+row=8
+# for i in range(1,row+1):
+#     print("* " * i)
+     
+# for i in range(row+1,0,-1):
+#     print("* " * i)
+ 
+
+for i in range(1,row+1):
+    print(" "*(row-i)+"* "*i)
+
+for i in range(row + 1,0,-1):
+    print(" " * (row - i) + "* " * i)    
